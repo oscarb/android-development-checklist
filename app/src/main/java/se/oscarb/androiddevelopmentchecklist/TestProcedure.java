386bb9acb77a313828a9f141ee8144bd448dd006
Oscar <git@oscarb.se>
2016-04-24 20:35:31 +0200

    Add TestProcedure model

---
+++ app/src/main/java/se/oscarb/androiddevelopmentchecklist/TestProcedure.java
@@ -0,0 +1,44 @@
+package se.oscarb.androiddevelopmentchecklist;
+
+/**
+ * Created by Oscar on 2016-04-24.
+ */
+public class TestProcedure {
+    /*
+        Instance variables
+     */
+    private String id;
+    private String type;
+    private String description;
+
+    /*
+        Constructors
+     */
+
+    public TestProcedure() {
+        id = null;
+        type = null;
+        description = null;
+    }
+
+    public TestProcedure(String id, String type, String description) {
+        this.id = id;
+        this.type = type;
+        this.description = description;
+    }
+
+    /*
+        Methods - Getters
+     */
+    public String getId() {
+        return id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
